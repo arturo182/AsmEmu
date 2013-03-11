a6d77fbaafe0d5c8d253f2d2f022821f38001435
arturo182 <arturo182@tlen.pl>
2013-03-11 21:39:17 +0100

    New example.

---
+++ bin/examples/division_hardcore.asm
@@ -0,0 +1,22 @@
+; ¿e co? ¿e pisanie liter jest do kitu i nie ufasz kompilatorowi
+; wolisz sam pisaæ kod prosto do pamiêci?
+; nie ma problemu!
+; mamy tu coœ specjalnie dla Ciebie:
+; dzielenie z reszt¹, wersja hardcore!
+
+; wynik w 3
+; reszta w 2
+
+0	20
+	7
+
+.code 10
+10	1000 
+	4001 
+	6017 
+	0103
+	2000 
+	8019 
+	5010 
+	1000 
+	2002
